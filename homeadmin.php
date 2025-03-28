7197c50b43d3e5543226851874443cbd1d880411
unknown <112202306985@mhs.dinus.ac.id>
2025-03-28 12:43:13 +0700

    Memperbarui file homeadmin.php

--- homeadmin.php
+++ homeadmin.php
@@ -1,5 +1,12 @@
-<?php 
-// session_start()
+<?php session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+    exit();
+}
+
+//memanggil file berisi fungsi2 yang sering dipakai
+require "fungsi.php";
+require "head.html";
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,27 +14,17 @@
 	<title>Halaman Home Admin</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">
+
 	<link rel="stylesheet" type="text/css" href="css/styleku.css">
-	<script src="bootstrap-5.3.3-dist/jquery/jquery-3.7.1.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap-5.3.3-dist/css/bootstrap.css">
 	<script src="bootstrap-5.3.3-dist/js/bootstrap.js"></script>
+	<script src="bootstrap-5.3.3-dist/jquery/jquery-3.7.1.min.js"></script>
+
 </head>
 <body>
-<?php
-//memanggil file berisi fungsi2 yang sering dipakai
-require "fungsi.php";
-require "head.html";
-
-//cek logout
-// if (!isset($_SESSION['username'])){
-	if (['username']){
-	header("location:index1.php");
-}
-?>
-<div class="utama">
-	<br><br>
-	<h1 class="text-center">Selamat Datang di Halaman Administrator saudara <?php echo strtoupper(['username'])?></h1>
-	<!-- h1 class="text-center">Selamat Datang di Halaman Administrator saudara //< ?php echo strtoupper($_SESSION['username'])?></h1 -->
-</div>
+    <div class="utama">
+	    <br><br>
+	    <h1 class="text-center">Selamat Datang di Halaman Administrator saudara <?php echo strtoupper($_SESSION['username']);?></h1>
+    </div>
 </body>
 </html>	
